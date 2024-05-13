8ef968d63c0c5f0ee9522ef017e44933bacc2bc4
Sri Sakthi <95374074+Sri-Sakthi-CB@users.noreply.github.com>
2024-05-13 18:32:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-var randomNumber1 = Math.floor( (Math.random() * 6)+1);
+var randomNumber1 = Math.floor((Math.random() * 6)+1);
 
-var imageSource1 = "images/dice" + randomNumber1 + ".png";
+var imageSource1 = "images/dice/" + randomNumber1 + ".png";
 
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", imageSource1);
 
 var randomNumber2 = Math.floor(Math.random()*6 + 1);
 
-var imageSource2 = "images/dice" + randomNumber2 + ".png";
+var imageSource2 = "images/dice/" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
 
